5ca79eef39a4910b278a07c3f944d36632e86311
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-09-30 02:21:49 +0600

    added navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../SharedPage/Navbar';
 
 const Home = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <h3>Home page</h3>
         </div>
     );
